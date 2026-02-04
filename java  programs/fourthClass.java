1acde37720fe00d95323cfa9b79516f04db7a4c3
revxi <138651309+revxi@users.noreply.github.com>
2026-02-04 09:23:17 +0000

    Add java programs and update repository

---
+++ java  programs/fourthClass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class fourthClass {
+    public static void main(String[] args){
+        // int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+            // int i =0;
+            // do{
+            //     System.out.println(i);
+            //     i++;
+            // }while(i<11);
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for(int i=1; i<11;i++){
+            System.out.println(n*i);
+            } 
+        }
+     } 
+
